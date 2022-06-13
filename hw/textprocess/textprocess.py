002fbe2811cd9c44a9c4119d26d2cc2b006e566b
yuhang zhu <zcolley96@gmail.com>
2022-06-12 21:13:25 -0400

    add more code

--- hw/textprocess/textprocess.py
+++ hw/textprocess/textprocess.py
@@ -3,15 +3,18 @@ text = '''
 "555-1239Dr. Bernard Lander(636) 555-0113Hollingdorp, Donnatella555-6542Fitzgerald, F. Sco\
 tt555 8904Rev. Martin Luther King636-555-3226Snodgrass, Theodore5553642Carlamina Scarfoni"
 '''
-# extract numbers
+# question 1 extract numbers
 numbers = re.findall(r'\d+', text)
-# print(numbers)
+print(numbers)
 
-# extract names
+# question 2 extract names
 names = re.findall('[A-Za-z.,]+(?: [A-Za-z.,]+)*', text)
+print(names)
+
+
+# question 3 arrange order
 
 new_names = []
-# arrange order
 for char in names:
     # print(char.split(' '))
     name = char.split(' ')
@@ -23,9 +26,9 @@ for char in names:
     else:
         new_names.append(char)
 
-# print(new_names)
+print(new_names)
 
-# 4 dr/dev
+# question 4 dr/dev
 #
 res = []
 for char in names:
@@ -35,18 +38,19 @@ for char in names:
     else:
         res.append(False)
 
-# print(res)
-
-# dr_rev = re.findall(r'(?:Dr|Rev)\.\s\w+\s\w+', text)
-# print(dr_rev)
+print(res)
 
 
-# 5  extract names
-# mid_name_and_last_name = re.findall(
-#     '/\w+([[:space:]])\w+([[:space:]])\w+$/gm', text)
-# print(mid_name_and_last_name)
+# question 5  extract names
+res_5 = []
+for name in new_names:
+    if len(name) >= 2:
+        res_5.append(True)
+    else:
+        res_5.append(False)
 
-# 6 html <title>+++BREAKING NEWS+++<title>
+print(res_5)
+# question 6 html <title>+++BREAKING NEWS+++<title>
 '''
 <.+> this will fail because . means any characters so it takes space and + etc
 so it will print whole string. correct regex is <\w+>  \w means character from a-z, 0 - 9 and under score. so this regex 
@@ -54,9 +58,9 @@ will extract first html tag.
 '''
 html_str = '<title>+++BREAKING NEWS+++<title>'
 html = re.findall('<\w+>', html_str)
-# print(html)
+print(html)
 
-# equation
+# question 7 equation
 equation_str = '(5-3)^2=5^2-2*5*3+3^2'
 res = re.findall(r'^\([0-9-^=*+)]+', equation_str)
 print(res)
